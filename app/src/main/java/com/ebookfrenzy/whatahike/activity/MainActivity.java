b07514d7a7ee90e5ea8a95a9c6702155f7da8648
chenched05 <chenched05@gmail.com>
2021-11-22 05:10:52 -0800

    search

--- app/src/main/java/com/ebookfrenzy/whatahike/activity/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/whatahike/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.ebookfrenzy.whatahike.activity;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,7 +10,12 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
 
 import com.ebookfrenzy.whatahike.Filter;
 import com.ebookfrenzy.whatahike.R;
@@ -35,13 +39,14 @@ import java.util.List;
 
 public class MainActivity extends BaseActivity {
     private ArrayList<trailRecord> trailList;
+    private RecyclerAdapter adapter;
     private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initSearchWidgets();
+
         recyclerView = findViewById(R.id.recyclerView);
         trailList = new ArrayList<>();
 
@@ -49,30 +54,7 @@ public class MainActivity extends BaseActivity {
         setAdapter();
     }
 
-    private void initSearchWidgets()
-    {
-        SearchView  searchView = (SearchView) findViewById(R.id.trailListSearchView);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                ArrayList<trailRecord> filteredTrails = new ArrayList<trailRecord>();
-                for(trailRecord trailrecord: trailList)  {
-                    if(trailrecord.getName().toLowerCase().contains(newText.toLowerCase())){
-                        filteredTrails.add(trailrecord);
-                    }
-                }
-                RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), 0,filteredTrails);
-                recyclerView.setAdapter(adapter);
-                return false;
-            }
-        });
-    }
     //if need to request permissions, extends BaseActivity and override function getRequestedPermissions()
     @Override
     String[] getRequestedPermissions() {
@@ -86,7 +68,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void setAdapter() {
-        RecyclerAdapter adapter = new RecyclerAdapter(trailList);
+        adapter = new RecyclerAdapter(trailList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -100,5 +82,29 @@ public class MainActivity extends BaseActivity {
         trailList.add(new trailRecord("4","Rainier", "It is the best place to hike", 9));
         trailList.add(new trailRecord("5","Olympic", "It is the good place to hike", 5));
         trailList.add(new trailRecord("6","North Cascade", "It is the wonderful place to hike", 2));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
     }
 }
--- app/src/main/java/com/ebookfrenzy/whatahike/utils/RecyclerAdapter.java
+++ app/src/main/java/com/ebookfrenzy/whatahike/utils/RecyclerAdapter.java
@@ -1,9 +1,10 @@
 package com.ebookfrenzy.whatahike.utils;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,24 +16,54 @@ import com.ebookfrenzy.whatahike.trailRecord;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> implements
+        Filterable {
     private ArrayList<trailRecord> trailList;
+    private ArrayList<trailRecord> trailListFull;
 
     public RecyclerAdapter(ArrayList<trailRecord> trailList){
         this.trailList = trailList;
+        trailListFull = new ArrayList<>(trailList); //create the copy oif trail list
     }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<trailRecord> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(trailListFull);
+            }
+            else{
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (trailRecord item : trailListFull) {
+                    if(item.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            trailList.clear();
+            trailList.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView nameTxt;
         private TextView descriptionTxt;
         private TextView idTxt;
         private TextView difficultyTxt;
-
-        public RecyclerAdapter(Context context, int resource, List<trailRecord> trailList)
-        {
-            super(context,resource,trailList);
-        }
-
 
         public MyViewHolder(final View view){
             super(view);
