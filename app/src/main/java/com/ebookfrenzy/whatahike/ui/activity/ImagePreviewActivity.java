cefaeb0c691d3208e21f39d070672a6d90d73c2b
yangyulu1114 <yangyulu1114@163.com>
2021-11-24 21:42:40 -0800

    add comment and image preview

---
+++ app/src/main/java/com/ebookfrenzy/whatahike/ui/activity/ImagePreviewActivity.java
@@ -0,0 +1,16 @@
+package com.ebookfrenzy.whatahike.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ebookfrenzy.whatahike.R;
+
+public class ImagePreviewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+    }
+}
