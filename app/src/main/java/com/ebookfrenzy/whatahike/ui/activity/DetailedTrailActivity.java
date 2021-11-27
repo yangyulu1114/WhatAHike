4183e265263e63e628913d8599bf77e8251898b2
zhiyulong <zhiyulong@email.arizona.edu>
2021-11-26 18:02:17 -0800

    add new activity for detailed trail

---
+++ app/src/main/java/com/ebookfrenzy/whatahike/ui/activity/DetailedTrailActivity.java
@@ -0,0 +1,17 @@
+package com.ebookfrenzy.whatahike.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ebookfrenzy.whatahike.R;
+
+
+public class DetailedTrailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_trail);
+    }
+}
